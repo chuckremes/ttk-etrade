d7bfb390dcc9d3e041b14290e8ba6130bf95e3d8
Chuck Remes <git@chuckremes.com>
2022-01-19 20:05:25 -0600

    for now restrict chains to Puts and certain number of strikes to reduce consumption of market data quota

--- lib/ttk/etrade/session/option_chains.rb
+++ lib/ttk/etrade/session/option_chains.rb
@@ -4,14 +4,15 @@
 class TTK::ETrade::Session::OptionChains < TTK::ETrade::Session::Base
   URL = "/v1/market/optionchains"
 
-  def reload(symbol, expiration)
+  def reload(symbol, expiration, callput: "PUT", strike_count: 70)
     params = {
       symbol: symbol,
       expiryYear: expiration.year,
       expiryMonth: expiration.month,
       expiryDay: expiration.day,
       includeWeekly: true,
-      chainType: "CALLPUT",
+      chainType: callput,
+      noOfStrikes: strike_count
     }
     result = get(URL, query_params: params)
     error_check(result, context: { symbol: symbol, expiration: expiration, query_params: params })
